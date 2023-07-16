773367ff3d42f9aa617532ed03e29ab73d3d0e80
Manas <manasmishramm31@gmail.com>
2023-07-16 16:34:30 +0530

    insertion sort in java

---
+++ Java/InsertionSort.java
@@ -0,0 +1,16 @@
+public class InsertionSort {
+    public static void main(String args[]) {
+        int arr[] = {5,4,7,2,9,1,3};
+        for(int i=1; i<arr.length; i++) {
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        for(int i=0; i<arr.length; i++)
+        System.out.print(arr[i]);
+    }
+}
